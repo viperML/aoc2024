e9d3f1957c32cc159533a228595415b37251ef5a
Fernando Ayats <ayatsfer@gmail.com>
2024-12-01 10:39:51 +0100

    day 1 part 2

--- app/Day1.hs
+++ app/Day1.hs
@@ -2,28 +2,34 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Day1 where
-import Data.Ord
+
 import Data.List
+import Data.Ord
+import Data.Functor
 
 day1 :: String -> IO ()
 day1 input = do
-    let l = parse input
+  let l = parse input
 
-    let l1 = sortBy (comparing fst) l
-    let l2 = sortBy (comparing snd) l
+  let l1 = sortBy (comparing fst) l
+  let l2 = sortBy (comparing snd) l
 
-    let x = zipWith (\(left, _) (_, right) -> abs (left - right)) l1 l2
-    print x
-    print $ sum x
+  let x = zipWith (\(a, _) (_, b) -> abs (a - b)) l1 l2
+  print x
+  print (sum x)
 
-    return ()
+day1part2 :: String -> IO ()
+day1part2 s = do
+  let l = parse s
 
+  let x = l <&> (\(a, _) -> a * toInteger (length (filter (\(_, b) -> b == a) l)))
+  print x
+  print (sum x)
 
 parse :: String -> [(Integer, Integer)]
 parse s = parseLine <$> lines s
-  where
-    parseLine line = (a, b)
-      where
-        w = read <$> words line
-        [a, b] = w
-
+ where
+  parseLine line = (a, b)
+   where
+    w = read <$> words line
+    [a, b] = w
--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,36 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Main where
 
 import Day1
 import Options.Applicative
 
 data Cli = Cli
-    { day :: Integer
-    , input_file :: FilePath
-    , part :: Integer
-    }
-    deriving (Show)
+  { day :: Integer
+  , input_file :: FilePath
+  , part :: Integer
+  }
+  deriving (Show)
 
 cli :: Parser Cli
 cli =
-    Cli
-        <$> option
-            auto
-            (long "day" <> short 'd')
-        <*> strOption (long "input-file" <> short 'i')
-        <*> option auto (long "part" <> short 'p')
+  Cli
+    <$> option
+      auto
+      (long "day" <> short 'd')
+    <*> strOption (long "input-file" <> short 'i')
+    <*> option auto (long "part" <> short 'p')
 
 main :: IO ()
 main = do
-    parsedCli <- execParser $ info (cli <**> helper) fullDesc
-    print parsedCli
+  parsedCli <- execParser $ info (cli <**> helper) fullDesc
+  print parsedCli
 
-    input <- readFile parsedCli.input_file
+  input <- readFile parsedCli.input_file
 
-    case (parsedCli.day, parsedCli.part) of
-        (1, 1) -> Day1.day1 input
-        _ -> putStrLn "Day not implemented"
+  case (parsedCli.day, parsedCli.part) of
+    (1, 1) -> Day1.day1 input
+    (1, 2) -> Day1.day1part2 input
+    _ -> putStrLn "Day not implemented"
 
-    return ()
+  return ()
