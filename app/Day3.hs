30469ca8c6f3bcccd6cb662d02eb241aa1c0dbc6
Fernando Ayats <ayatsfer@gmail.com>
2024-12-03 22:51:08 +0100

    tweak

--- app/Day3.hs
+++ app/Day3.hs
@@ -10,9 +10,7 @@ import Text.Megaparsec.Char
 type Parser = ParsecT Void T.Text (State Bool)
 
 number :: Parser Integer
-number = do
-    s <- some digitChar
-    return (read s)
+number = read <$> some digitChar
 
 mul :: Parser Integer
 mul = do
@@ -30,17 +28,14 @@ doParser :: Parser ()
 doParser = (string "do()" >> put True) <|> (string "don't()" >> put False)
 
 p1 :: Parser [Integer]
-p1 = some $ try (skipManyTill anySingle (try mul))
+p1 = many $ try (skipManyTill anySingle (try mul))
 
 p2 :: Parser [Integer]
 p2 = many $ try (skipManyTill (doParser <|> void anySingle) (try mul))
 
 dayFor :: Parser [Integer] -> String -> IO ()
 dayFor p input = do
-    let i = T.pack input
-    print i
-
-    res <- case runState (runParserT p "" i) True of
+    res <- case runState (runParserT p "" (T.pack input)) True of
         (Right r, s) -> print s >> return r
         (Left err, s) -> error (errorBundlePretty err)
 
@@ -50,5 +45,5 @@ dayFor p input = do
 day3 :: String -> IO ()
 day3 = dayFor p1
 
-day3part2 ::String -> IO ()
+day3part2 :: String -> IO ()
 day3part2 = dayFor p2
