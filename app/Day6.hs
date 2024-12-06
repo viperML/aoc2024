1ad30c6911419d6f37f45b61c5c7c794ecf42650
Fernando Ayats <ayatsfer@gmail.com>
2024-12-06 22:35:32 +0100

    day 6 part 1

--- app/Day6.hs
+++ app/Day6.hs
@@ -1,12 +1,66 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module Day6 (day6, day6part2) where
 
+import Control.Monad
+import Control.Monad.Writer.Lazy
+import Data.Array ((!))
+import Data.Array.IArray ((!?))
+import qualified Data.Array.IArray as A
+import Data.Function ((&))
+import Data.List (nub, nubBy)
+import Linear (V2 (..))
 import Text.Show.Pretty (pPrint)
-import Util (parseMapChar)
+import Util (Map, parseMapChar)
+import Prelude hiding (Left, Right, map)
+
+data Direction = Up | Down | Left | Right
+    deriving (Show, Eq)
+
+rotate :: Direction -> Direction
+rotate = \case
+    Up -> Right
+    Right -> Down
+    Down -> Left
+    Left -> Up
+
+data Position = Position
+    { coord :: V2 Int
+    , dir :: Direction
+    }
+    deriving (Show, Eq)
+
+runPos :: Position -> Position
+runPos pos = Position newCoord pos.dir
+  where
+    newCoord =
+        pos.coord + case pos.dir of
+            Up -> V2 (-1) 0
+            Down -> V2 1 0
+            Left -> V2 0 (-1)
+            Right -> V2 0 1
 
 day6 :: String -> IO ()
 day6 input = do
-    let parsed = parseMapChar input
-    pPrint parsed
+    let initialPos = Position (fst $ head $ filter (\e -> snd e == '^') (A.assocs charMap)) Up
+    let steps = go initialPos
+    pPrint $ length (nubBy (\a b -> a.coord == b.coord) steps)
+  where
+    charMap = parseMapChar input
+    go pos = case sim charMap pos of
+        Just next -> next : go next
+        Nothing -> []
+
+sim :: Map Char -> Position -> Maybe Position
+sim map curPos = do
+    let nextPos = runPos curPos
+    _ <- map !? nextPos.coord
+    let nextNextPos = runPos nextPos
+    case map !? nextNextPos.coord of
+        Just '#' -> Just $ Position nextPos.coord (rotate nextPos.dir)
+        _ -> Just nextPos
 
 day6part2 :: String -> IO ()
 day6part2 input = do
--- app/Util.hs
+++ app/Util.hs
@@ -3,19 +3,22 @@ module Util where
 import Debug.Trace (traceShow)
 
 import Data.Array.IArray as A
+import Linear (V2 (V2))
 
 traceVal :: (Show a) => a -> a
 traceVal x = traceShow x x
 
-parseMap :: (Char -> a) -> String -> A.Array (Int, Int) a
+type Map a = A.Array (V2 Int) a
+
+parseMap :: (Char -> a) -> String -> Map a
 parseMap mapping = fmap mapping . parseMapChar
 
-parseMapChar :: String -> A.Array (Int, Int) Char
-parseMapChar s = A.array ((0, 0), (maxlines, maxchars)) $ do
+parseMapChar :: String -> Map Char
+parseMapChar s = A.array (V2 0 0, V2 maxlines maxchars) $ do
     (iline, line) <- zip [0 ..] (lines s)
     (ielem, c) <- zip [0 ..] line
 
-    return ((iline, ielem), c)
+    return (V2 iline ielem, c)
   where
     maxlines = length (lines s) - 1
     maxchars = length (head $ lines s) - 1
